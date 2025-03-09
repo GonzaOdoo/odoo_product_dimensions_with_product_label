b9577548bca53b2ea0957e8ddf98925613c2bd96
EmilianoSuarezDeglia <emilianosuarez@deglia.xyz>
2025-03-09 13:28:25 -0300

    Módulo de personalización de etiquetas V2

---
+++ product_label_custom/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Personalización de etiquetas',
+    'version': '1.0',
+    'summary': 'Modifica la vista de etiquetas de productos incluyendo Largo, Ancho, Alto, Volumen, Código de barra y Número de lote.',
+    'author': 'Deglia',
+    'depends': ['product', 'mrp'],
+    'data': [
+        'views/product_label_custom.xml',  
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
